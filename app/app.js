475ee62e39f87366fca6268ae771d0e51b8b6294
blandine <bdescamps@viareport.com>
2014-03-18 11:52:16 +0100

    FIX app.js : needs db.js require

--- app/app.js
+++ app/app.js
@@ -1,10 +1,11 @@
 var srv = require('./server');
+var db = require('./db');
 
 var ip = process.env.IP || '127.0.0.1';
 var port = process.env.PORT || 3001;
 
 db.open(function(err, db) {
     if (err) throw err;
-    
-    srv.listen(port, ip);    
+
+    srv.listen(port, ip);
 });
